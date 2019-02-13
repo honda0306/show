529719c91a273a1d5d91c74cac709b7776e89755
Chris Honda <christopher.honda92@gmail.com>
2019-02-13 14:38:27 -0800

    Moved attribution to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
+        <p className="copyright">A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
         <p className="copyright">&copy; Gatsby Starter - Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
     </footer>
 )
